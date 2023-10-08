2073ea02e2bacd7945b95e11beaca142ae6d8ab1
RenukaK10 <renuka.khanzode10@gmail.com>
2023-10-08 16:24:22 +0530

    Data Type Intro

---
+++ 1_data_types/Intro.py
@@ -0,0 +1,10 @@
+name = "Renuka"
+id = 100
+salary = 10000.50
+
+print("my name is:",name)
+print("my id is:",id)
+print("her salary is:",salary) 
+print(type(name))
+print(type(id))
+print(type(salary))
