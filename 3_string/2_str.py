889e26ae398fea87190b2d403ff7266222cc0fe9
RenukaK10 <renuka.khanzode10@gmail.com>
2023-10-14 13:17:11 +0530

    string and boolean data type

---
+++ 3_string/2_str.py
@@ -0,0 +1,13 @@
+#string data type with boolean values
+s1=600
+s2 = 300
+print(s1 is s2)
+#this gives the result in boolean type 
+
+a = "hello"
+b = "hello"
+c = "hi"
+print(a==b)
+print(a==c)
+
+#is and == are similar and they give result in boolean type
