702dd7a79dae110d7a230e019499b27768588d3c
RenukaK10 <renuka.khanzode10@gmail.com>
2023-11-13 13:23:35 +0530

    Creating and calling a function

---
+++ 4_functions/creating_func.py
@@ -0,0 +1,13 @@
+def display():
+	print("Welcome to function concept")
+display()
+
+# Function callign is independent of its sequential assignment
+
+def first():
+	print("this is first function")
+def second():
+	print("this is second function")
+
+second()
+first()
